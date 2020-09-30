235681465d7a2eec03c6ce8f9d5ca896d23d98df
Gabe <watson@chesapeakecommons.org>
2020-09-30 16:59:03 -0400

    Extent selection pulldown and deleting unneeded code

--- app.R
+++ app.R
@@ -1,8 +1,9 @@
 # Save the Sound's 2020 Action Map
 # Documentation https://docs.google.com/document/d/14kDLOjaSYhNqzDJXG9Zn2SWk5T-k_RPIiF2jXS9h_zA/edit?usp=sharing
+# Github: https://github.com/ChesapeakeCommons/R-Shiny
 # Created by Gabe Watson for The Commons 
 # Created 7.20.2020 
-# Last edited: 7.20.2020 
+# Last edited: 9.21.2020 
 # Naming Convention  
 #   - Major_Components_Name minor_componentsName, MajorVariable minorvariable 
 #   
@@ -16,7 +17,8 @@ library(leaflet.extras)
 library(htmltools)
 
 ###########  UI Display Script ############
-ui <- fluidPage(theme = "styler.css",
+ui <- fluidPage(
+#(theme = "styler.css",
 
 # Test
            
@@ -24,12 +26,19 @@ ui <- fluidPage(theme = "styler.css",
   
   #Map
   div(id = "wrapper",
+      
+      
+      
+      #Layer Selection
+      selectizeInput("inActionSelector", "Input checkbox",
+                     c("Loading Layers"), multiple = TRUE),
 
 
       div(id = "main-panel",
           leafletOutput("map")
-          )
+         )
 
+      # Side Panel Display with Save the Sound Info
       ),
       div(id = "side-panel",
          div( id = "side-panel-wrapper",
@@ -37,6 +46,8 @@ ui <- fluidPage(theme = "styler.css",
                     img(    id = 'title-image',
                             src='images/save-the-sound-title.png'
                     )
+                    
+                    #Descriptive Text 
               ),
               div(  class = "description",
                     tags$h1("2020 Action Map"),
@@ -53,6 +64,8 @@ ui <- fluidPage(theme = "styler.css",
                     tags$p(
                         HTML("<font style='font-weight: 400;'>Efforts to improve the</font> Long Island Sound")
                    )
+                   
+                   # Zoom Controls 
               ),
               div(  id = "action-panel",
                  #Map extent switching buttons
@@ -69,21 +82,11 @@ ui <- fluidPage(theme = "styler.css",
               )
          )
       )
-
- 
-
-     
    )
-
-      
-  
-
-  
-  
 
 ########## Server Side Script ############
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   ##### DATASET IMPORT ####
   # this lets us bypass the Authorization step from gsheet4 if they are ok with just having the googlesheet be public - I can hook it up to be private though after the fact.
@@ -92,28 +95,38 @@ server <- function(input, output) {
   Import_V1 <- read_sheet("https://docs.google.com/spreadsheets/d/1pZBhwo97IHkp-bNCA-m9UBJyBsO9ngnyuFylfAMbZs4/edit#gid=0")
   
   Symbology_V1 <- read_sheet("https://docs.google.com/spreadsheets/d/1N0L7-gZH4iqxrbQVYLtLJU7Dbuz2nVnYE-8wUrzPK6o/edit#gid=0")
-  
-
-  #Joining Import and Symbology and then selecting down to each particular Layer
-  Map_Data_All <- Import_V1 %>%
-    left_join(Symbology_V1)
-  
-  #Creating the 5 Main Layers and their Icons
-  
-  #Legal Actions
-  map_data_LegAct <- Map_Data_All %>%
-    filter(Action == 'Legal Actions')
-  
-  # Pollution Actions
-  map_data_PollAct <- Map_Data_All %>%
-    filter(Action == 'Pollution Actions')
-
-  
   #### END DATASETIMPORT #####
   
   
+  #### ACTION INPUT DATA HANDLING ###
   
-  #### INPUT DATA HANDLING ###
+  # Updates the Action selector once the input sheets are handled above 
+  observe({
+    updateSelectizeInput(session, "inActionSelector",
+                         choices = Import_V1$Action)
+  })
+  
+  # Reactive element that gets the correct Layer Selection and returns "ActionSelection()" for use in mapping from Action Selector
+  ActionSelection <- reactive ({
+    
+    #Converts the input datatype into list --- I think...
+    y <- input$inActionSelector
+    
+    #Sets dataframe to all Actions if no selection is chosen
+    if (is.null(y))
+       {
+      Layers <- Import_V1
+      return(Layers)
+    }
+    #Sets dataframe to selected Actions
+    else
+      {
+    Layers <-filter(Import_V1, Action %in% y)
+    return(Layers)
+      }
+    
+  })
+  
   
   #Gets the selected zoom extent
   
@@ -156,87 +169,113 @@ server <- function(input, output) {
     
     Io_Data <- Io_Map_Click()  
     write.csv(Io_Data, "Io_Data.csv")
-    head(Io_Data)},
-    
-  )
-  
+    head(Io_Data)
+    })
   #### END INFORMATION PANEL ####
   
   
-  
-  #### MAP ####
+  ##### MAP ####
   output$map <- renderLeaflet({
     
+ # ActionLayers <- LayerSelection()
+  
+#  write.csv(ActionLayers, "ActionLayers2.csv")
+    
+  groups = as.character(unique(ActionSelection()$Action)) 
+  
+  map = leaflet(ActionSelection()) %>%
+    
+  addTiles(group = "OpenStreetMap")
  
-    # map extent logic 
-    if (is.null(extent$data))
-    {
-      extent$data$lat <- 41.314902
-      extent$data$long <- -82.481357
-      extent$data$zoom <- 10
-    }
+  #Loop that runs through dataframe to 
+   for(g in groups)
+   {
+     d = ActionSelection()[ActionSelection()$Action == g, ]
+ 
+     map = map %>% addCircleMarkers(data = d, lng = ~longitude, lat = ~latitude,
+                                  color = 277706L,
+                                  group = g)
+     }
+      map %>% addLayersControl(overlayGroups = groups)
+ })
     
-    
-    #Icons 
-    iconTest <- makeIcon(
-      iconUrl = "images/logo.svg",
-      iconWidth = 40, iconHeight = 95
-     # iconAnchorX = 0, iconAnchorY = 0,
-    )
-    
-     iconTest2 <- makeIcon(
-       iconUrl = "images/greydrop.png",
-       iconWidth = 10, iconHeight = 15
-       # iconAnchorX = 0, iconAnchorY = 0,
-    )
-    
-    leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
-       htmlwidgets::onRender("function(el, x) {
-        L.control.zoom({ position: 'topright' }).addTo(this)
-    }") %>%
-      
-      #Setting default extent and zoom based on user selection. 
-      setView(lng = extent$data$long, lat = extent$data$lat, zoom = extent$data$zoom)%>%
-      
-      #Command to add Circle Markers as layers
-      addTiles() %>%
-
-      # Adding Satisfactory, Compliance 
-      addMarkers(data = map_data_LegAct, lng = ~longitude, lat = ~latitude, icon = iconTest,
-                     #  color = map_data_LegAct$MapColor, 
-                       label = ~ as.character(map_data_LegAct$Action_Name),
-                    #   stroke = FALSE, fillOpacity = 0.5,
-                       clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
-                       group = "Legal Actions")%>%
-      
-      # Adding Non Compliance
-      addMarkers(data = map_data_PollAct, lng = ~longitude, lat = ~latitude,icon = iconTest2, popup = ~htmlEscape(map_data_PollAct$Volunteers),
-                      # color = map_data_PollAct$MapColor, 
-                       label = ~ as.character(map_data_PollAct$Action_Name),
-                      # stroke = FALSE, fillOpacity = 0.5,
-                       clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
-                       group = "Pollution Actions")%>%
-
-      #Adding Basemap 
-      addProviderTiles(providers$CartoDB.Voyager, group = "Streets")%>%
-      addProviderTiles(providers$Esri.NatGeoWorldMap, group = "Satellite")%>%
-
-      #Adding geocoder
-      addSearchOSM(options = searchOptions(zoom=15, position = 'topright',
-                                            autoCollapse = TRUE,
-                                            minLength = 2)) %>%
-
-      #Adding layer control 
-      addLayersControl(
-        baseGroups = c("Streets", "Satellite"),
-        overlayGroups = c("Legal Actions", "Pollution Actions"),
-        options = layersControlOptions(collapsed = FALSE, position = 'bottomright')
-      )
-      
-    
-  })
   
+  #### END MAP #####
+    
   
+  #### OLD MAP WILL DELETE AFTER HARVESTING CODE ####
+  # output$map <- renderLeaflet({
+  #   
+  # 
+  #   # map extent logic 
+  #   if (is.null(extent$data))
+  #   {
+  #     extent$data$lat <- 41.314902
+  #     extent$data$long <- -82.481357
+  #     extent$data$zoom <- 10
+  #   }
+  #   
+  #   
+  #   #Icons 
+  #   iconTest <- makeIcon(
+  #     iconUrl = "images/logo.svg",
+  #     iconWidth = 40, iconHeight = 95
+  #    # iconAnchorX = 0, iconAnchorY = 0,
+  #   )
+  #   
+  #    iconTest2 <- makeIcon(
+  #      iconUrl = "images/greydrop.png",
+  #      iconWidth = 10, iconHeight = 15
+  #      # iconAnchorX = 0, iconAnchorY = 0,
+  #   )
+  #   
+  #   leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
+  #      htmlwidgets::onRender("function(el, x) {
+  #       L.control.zoom({ position: 'topright' }).addTo(this)
+  #   }") %>%
+  #     
+  #     #Setting default extent and zoom based on user selection. 
+  #     setView(lng = extent$data$long, lat = extent$data$lat, zoom = extent$data$zoom)%>%
+  #     
+  #     #Command to add Circle Markers as layers
+  #     addTiles() %>%
+  # 
+  #     # Adding Satisfactory, Compliance 
+  #     addMarkers(data = map_data_LegAct, lng = ~longitude, lat = ~latitude, icon = iconTest,
+  #                    #  color = map_data_LegAct$MapColor, 
+  #                      label = ~ as.character(map_data_LegAct$Action_Name),
+  #                   #   stroke = FALSE, fillOpacity = 0.5,
+  #                      clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
+  #                      group = "Legal Actions")%>%
+  #     
+  #     # Adding Non Compliance
+  #     addMarkers(data = map_data_PollAct, lng = ~longitude, lat = ~latitude,icon = iconTest2, popup = ~htmlEscape(map_data_PollAct$Volunteers),
+  #                     # color = map_data_PollAct$MapColor, 
+  #                      label = ~ as.character(map_data_PollAct$Action_Name),
+  #                     # stroke = FALSE, fillOpacity = 0.5,
+  #                      clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F),
+  #                      group = "Pollution Actions")%>%
+  # 
+  #     #Adding Basemap 
+  #     addProviderTiles(providers$CartoDB.Voyager, group = "Streets")%>%
+  #     addProviderTiles(providers$Esri.NatGeoWorldMap, group = "Satellite")%>%
+  # 
+  #     #Adding geocoder
+  #     addSearchOSM(options = searchOptions(zoom=15, position = 'topright',
+  #                                           autoCollapse = TRUE,
+  #                                           minLength = 2)) %>%
+  # 
+  #     #Adding layer control 
+  #     addLayersControl(
+  #       baseGroups = c("Streets", "Satellite"),
+  #       overlayGroups = c("Legal Actions", "Pollution Actions"),
+  #       options = layersControlOptions(collapsed = FALSE, position = 'bottomright')
+  #     )
+  #     
+  #   
+  # })
+  
+  # TESTING TEXT OUTPUT - WILL BE TURNED INTO POPUP INFORMATION
   
   output$mapclick <- renderText({ 
     #Rreturns text of Map click
